836ba44600592c905dea41e454a66bd7a1f163c9
Dave Avent <davent@yelp.com>
2016-09-13 13:31:00 +0100

    Added logging

--- aerospike_cache/cache.py
+++ aerospike_cache/cache.py
@@ -239,7 +239,7 @@ class AerospikeCache(BaseCache):
         logging.debug("[{4}] Trying to put: {0}, {1}, {2}, {3}".format(aero_key, record, meta, self.policy, self._client))
         logging.debug("Connected? {0}".format(self._client.is_connected()))
         if self._client.is_connected() != True:
-          self._client.Connect()
+          self._client.connect()
         ret = self._client.put(aero_key, record, meta, self.policy)
 
         if ret == 0:
@@ -257,7 +257,7 @@ class AerospikeCache(BaseCache):
             logging.debug("[{0}] Trying to get: {1}, {2}".format(self._client, aero_key,self.policy))
             logging.debug("Connected? {0}".format(self._client.is_connected()))
             if self._client.is_connected() != True:
-              self._client.Connect()
+              self._client.connect()
             (key, metadata, record) = self._client.get(aero_key,self.policy)
             if record is None:
                 return default
